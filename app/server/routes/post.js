c20ed8e72b2b7fdcca9698276ac52a260520ea28
nisabmohd <nisabmohd.com>
2022-06-03 23:30:55 +0530

    like comment retweet hashtag routes added

--- app/server/routes/post.js
+++ app/server/routes/post.js
@@ -2,19 +2,19 @@ const express = require('express')
 const router = express.Router()
 const postModel = require('../models/Post')
 const { v4: postid } = require('uuid');
-const {signUpModel}=require('../models/Auth')
+const { signUpModel } = require('../models/Auth')
 //new post
 router.post('/newpost', async (req, res) => {
     try {
-        const getUser=await signUpModel.findOne({uid:req.body.uid})
+        const getUser = await signUpModel.findOne({ uid: req.body.uid })
         const newpost = new postModel({
             uid: req.body.uid,
             post_id: postid(),
             caption: req.body.caption,
             image: req.body.image ? req.body.image : "",
             hashtag: req.body.hashtag && req.body.hashtag.length != 0 ? req.body.hashtag : [],
-            userimg:getUser.userimg,
-            username:getUser.username
+            userimg: getUser.userimg,
+            username: getUser.username
         })
         const result = await newpost.save()
         res.status(200).send({ status: "success" })
@@ -26,7 +26,7 @@ router.post('/newpost', async (req, res) => {
 //all post
 router.get('/allpost', async (req, res) => {
     try {
-        const allpost = await postModel.find().sort({timestamp: -1});
+        const allpost = await postModel.find().sort({ timestamp: -1 });
         res.send(allpost)
 
     } catch (err) {
@@ -50,7 +50,7 @@ router.get('/:id', async (req, res) => {
 //user post
 router.get('/up/:uid', async (req, res) => {
     try {
-        const post = await postModel.find({ uid: req.params.uid }).sort({timestamp: 0});
+        const post = await postModel.find({ uid: req.params.uid }).sort({ timestamp: 0 });
         res.status(200).send(post)
 
     } catch (err) {
@@ -63,35 +63,98 @@ router.get('/up/:uid', async (req, res) => {
 //delete post
 router.delete('/:id', async (req, res) => {
     try {
-        const postDetails=await postModel.findOne({post_id:req.params.id})
-        if(!postDetails){
-            return res.status(400).send({status:"Post not found"})
+        const postDetails = await postModel.findOne({ post_id: req.params.id })
+        if (!postDetails) {
+            return res.status(400).send({ status: "Post not found" })
         }
-        if(postDetails.uid==req.body.uid){
+        if (postDetails.uid == req.body.uid) {
             const post = await postModel.deleteOne({ post_id: req.params.id });
+            return res.send({ status: "deleted" })
         }
-        res.send({status:"deleted"})
-
+        else {
+            return res.status(401).send({ status: "You are not authorised to delete the post" })
+        }
     } catch (err) {
-        res.status(400).send({status:"You are not authorised to delete the post"})
+        res.status(401).send(err)
+    }
+})
+
+
+//post of a hastag
+router.get('/hashtag/:hashtag', async (req, res) => {
+    try {
+        const data = await postModel.find({ hashtag: req.params.hashtag })
+        console.log(data);
+        res.send("sdfsfdgdfgddf")
+    } catch (err) {
+        res.status(400).send("sdfsdf")
     }
 })
 
 //like post
-
+router.put('/like/:postid', async (req, res) => {
+    try {
+        const result=await postModel.updateOne({ post_id: req.params.postid },{$push:{likes:req.body.uid}})
+        res.send(result)
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
 
 
 //unlike post
-
+router.put('/unlike/:postid', async (req, res) => {
+    try {
+        const result=await postModel.updateOne({ post_id: req.params.postid },{$pull:{likes:req.body.uid}})
+        res.send(result)
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
 
 
 //comment
-
+router.put('/comment/:postid', async (req, res) => {
+    try {
+        const result=await postModel.updateOne({ post_id: req.params.postid },{$push:{comments:{uid:req.body.uid,comment:req.body.comment}}})
+        res.send(result)
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
 
 
 //retweet
+router.put('/retweet/:postid', async (req, res) => {
+    try {
+        const result=await postModel.updateOne({ post_id: req.params.postid },{$push:{retweet:req.body.uid}})
+        res.send(result)
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
 
+
+//save apost in user
+
+
+//get saved post of user
+router.get('/saved/:uid',async(req,res)=>{
+    try{
+        const getAllSaveduid=await signUpModel.findOne({uid:req.params.uid})
+        const allsavedpost=[]
+        getAllSaveduid.saved.map(async(item)=>{
+            const temppost=await postModel.findOne({post_id:item})
+            allsavedpost.push(temppost);
+        })
+        res.send(allsavedpost)
+    }catch(err){
+        res.status(400).send(err)
+    }
+})
 
 //timeline posts
+
+
 
 module.exports = router
--- app/server/routes/user.js
+++ app/server/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const {signUpModel:userModel} = require('../models/Auth')
 
-
+//specific user
 router.get('/:uid', async (req, res) => {
     try {
         const result = await userModel.findOne({ uid: req.params.uid },{password:0})
@@ -11,5 +11,7 @@ router.get('/:uid', async (req, res) => {
         res.status(400).send(err)
     }
 })
+
+//
 
 module.exports = router

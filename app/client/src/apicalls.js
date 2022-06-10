e9c00670dcf45e413f04fdd1b7ad25f73bb063c1
nisabmohd <nisabmohd.com>
2022-06-10 17:32:29 +0530

    api wrap

--- app/client/src/apicalls.js
+++ app/client/src/apicalls.js
@@ -1,4 +1,4 @@
-// const baseurl=process.env.React_App_url
-const baseurl="http://localhost:5000"
+const baseurl=process.env.React_App_url
+// const baseurl="http://localhost:5000"
 
 export {baseurl};
